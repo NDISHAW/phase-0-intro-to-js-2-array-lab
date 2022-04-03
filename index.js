255be527ecb4ff542bcd2027c9cdefeebe837992
NDICHU <ndichumuriithi@gmail.com>
2022-04-03 18:05:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+}
+console.log(cats);
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+    console.log(cats);
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop();
+    console.log(cats);
+}
+// destructivelyRemoveLastCat();
+function  destructivelyRemoveFirstCat(name) {
+    cats.shift();
+    console.log(cats);
+}
+function appendCat(name){
+    const copyofCats=[...cats,"Broom"]
+    return copyofCats;
+}
+appendCat();
+
+function prependCat(name){
+    const copyOfCats=["Arnold", ...cats];
+    return copyOfCats;
+}
+prependCat();
+
+function removeLastCat(name){
+    const kanyau=[...cats];
+    kanyau.pop();
+    return kanyau;
+    
+}
+removeLastCat();
+
+function removeFirstCat(name){
+    const kamiau=[...cats];
+    kamiau.shift();
+    return kamiau;
+}
+removeFirstCat();
+console.log(kamiau);
